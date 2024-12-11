62cadb241904d85a9506ee062b578f4518902ffa
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2024-12-11 16:04:57 -0300

    criando a conexão com o banco de dados usando o PDO

--- classes/Produto.php
+++ classes/Produto.php
@@ -1,2 +1,15 @@
 <?php
 
+class Produto{
+    public function ListarProdutos(){
+        $conn = new PDO('mysql:host=localhost;dbname=db_police','root','');
+
+        $query = 'SELECT * FROM db_produto';
+
+        $resultado = $conn->query($query)->fetchAll();
+
+
+        // unset($conn); = "fecha" a conexão com o banco.
+        return $resultado;
+    }
+}
