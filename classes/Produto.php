e729267b814ff80d77549077a654fd780c77501f
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2024-12-11 17:10:18 -0300

    Termino das configurações dos preções e avaliações dos produtos

--- classes/Produto.php
+++ classes/Produto.php
@@ -4,7 +4,7 @@ class Produto{
     public function ListarProdutos(){
         $conn = new PDO('mysql:host=localhost;dbname=db_police','root','');
 
-        $query = 'SELECT * FROM tb_produto';
+        $query = 'SELECT * FROM tb_produto ORDER BY RAND() LIMIT 8';
 
         $resultado = $conn->query($query)->fetchAll();
 
--- includes/produto.php
+++ includes/produto.php
@@ -2,23 +2,27 @@
     <h2 class="titulo">Produtos</h2>
     <main class="container produtos">
         <div class="row">
-            <?php foreach($dadosProdutos as $dados){?>
-            <div class="col-3">
-                <figure>
-                    <img src="./assets/img/poster/<?= $dados['imagem']?>" alt="poster Assassin's Creed Mirage" class="foto-produto">
-                    <figcaption>
-                        <h4><?= $dados['titulo']?></h4>
-                        <span class="preco"><?= $dados['preco']?></span>
-                        <span class="avaliacao">
-                            <?php
-                            for ($j = 0; $j < 5; $j++){
-                                echo '<i class="bi bi-star-fill"></i>';
-                            }
-                            ?>
-                        </span>
-                    </figcaption>
-                </figure>
-            </div>
+            <?php foreach ($dadosProdutos as $dados) { ?>
+                <div class="col-3">
+                    <figure>
+                        <img src="./assets/img/poster/<?= $dados['imagem'] ?>" alt="poster Assassin's Creed Mirage" class="foto-produto">
+                        <figcaption>
+                            <h4><?= $dados['titulo'] ?></h4>
+                            <span class="preco">R$<?= number_format($dados['preco'], 2, ",", ".") ?></span>
+                            <span class="avaliacao">
+                                <?php
+                                for ($j = 1; $j <= 5; $j++) {
+                                    if ($j <= $dados['avaliacao']) {
+                                        echo '<i class="bi bi-star-fill"></i>';
+                                    }else{
+                                        echo '<i class="bi bi-star"></i>';
+                                    }
+                                }
+                                ?>
+                            </span>
+                        </figcaption>
+                    </figure>
+                </div>
             <?php } ?>
         </div>
     </main>
